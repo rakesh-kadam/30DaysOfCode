3998586aff637ddbdef211200252f65963611619
Hacker@135 <rakeshkadam26498@gmail.com>
2019-08-25 13:38:50 +0530

    solution

---  Day 1: Data Types/solution.py
+++  Day 1: Data Types/solution.py
@@ -2,18 +2,21 @@ i = 4
 d = 4.0
 s = 'HackerRank '
 # Declare second integer, double, and String variables.
-x = 12
-y = 4.0
-t = "is the best place to learn and practice coding!"
+i2 = None
+d2 = None
+s2 = None
 
 # Read and save an integer, double, and String to your variables.
+i2 = int(input())
+d2 = float(input())
+s2 = str(input())
 
 # Print the sum of both integer variables on a new line.
-print(x+i)
+print(i + i2)
 
 # Print the sum of the double variables on a new line.
-print(d+y)
+print(d + d2)
 # Concatenate and print the String variables on a new line
 # The 's' variable above should be printed first.
 
-print(s+t)
+print(s + s2)
