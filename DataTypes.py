f232c0e92fc9da3a069ed81a6c225e208b039b4d
Hacker@135 <rakeshkadam26498@gmail.com>
2019-08-25 13:24:25 +0530

    Day1

---
+++ DataTypes.py
@@ -0,0 +1,19 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+x = 12
+y = 4.0
+t = "is the best place to learn and practice coding!"
+
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+print(x+i)
+
+# Print the sum of the double variables on a new line.
+print(d+y)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+
+print(s+t)
